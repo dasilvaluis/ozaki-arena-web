b4249404b70a3ecfee147a9f6be1b74692c55873
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-12-03 22:39:17 +0200

    Add revalidate endpoint

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  revalidated: boolean
+  message?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (req.query.secret !== process.env.PAGE_REVALIDATE_TOKEN) {
+    return res.status(401).json({ revalidated: false, message: 'Invalid token.' });
+  }
+
+  if (typeof req.query.path !== 'string') {
+    return res.status(422).json({ revalidated: false, message: 'Invalid path. Expected path as a string.' });
+  }
+
+  try {
+    await res.revalidate(req.query.path);
+
+    return res.json({
+      revalidated: true,
+    });
+  } catch (err) {
+    return res.status(500).send({ revalidated: false, message: 'Error revalidating' });
+  }
+}
