25078307c6cab0eda096a03162c0dfa04c318c36
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-12-14 01:08:42 +0200

    Add coming soon to leader board

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,4 +1,9 @@
+import { TimeIcon } from '@chakra-ui/icons';
+import {
+  Box, Button, Center, HStack, Icon, Text
+} from '@chakra-ui/react';
 import PageTemplate from 'components/templates/page-template';
+import Link from 'next/link';
 import { ImageMeta } from 'types';
 
 type Props = {
@@ -12,7 +17,15 @@ export default function Leaderboard({ heroImage }: Props) {
       subTitle="Treci la nivelul următor"
       heroImage={heroImage}
     >
-      Content
+      <Center py="6rem" flexDirection="column">
+        <HStack>
+          <Text as="b" color="secondary" fontSize="3xl" textTransform="uppercase" mb={0}>in curand</Text>
+          <Icon boxSize={6} as={TimeIcon} color="primary" />
+        </HStack>
+        <Box mt="1rem">
+          <Link href="/"><Button colorScheme="orange">Acasă</Button></Link>
+        </Box>
+      </Center>
 
     </PageTemplate>
   );
