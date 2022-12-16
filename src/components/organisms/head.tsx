2e26ddc11be58d55730d018383df30c0c8c127d1
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-12-16 15:45:30 +0200

    Add opengraph tags

--- src/components/organisms/head.tsx
+++ src/components/organisms/head.tsx
@@ -9,8 +9,18 @@ export default function Head({
 }: Props) {
   return (
     <NextHead>
-      <title>{`Ozaki | ${title}`}</title>
-      <meta name="description" content="Ozaki Arena | Sala de Cățărat" />
+      <title>{`Ozaki Arena | ${title}`}</title>
+      <meta
+        name="description"
+        content="Ozaki Arena - Sala ta Cățărat în Sibiu. Escalada, training, fun walls, guidare și instructori."
+      />
+      <meta property="og:title" content={`Ozaki Arena | ${title}`} />
+      <meta property="og:site_name" content="Ozaki Arena" />
+      <meta property="og:url" content="ozakiarena.ro" />
+      <meta property="og:description" content="Sala ta Cățărat în Sibiu. Escalada, training, fun walls, guidare și instructori." />
+      <meta property="og:type" content="business.business" />
+      <meta property="og:image" content="" />
+
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
